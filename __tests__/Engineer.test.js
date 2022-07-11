9eb3483795d33a87d489091755b940f7a7582f35
robpduo <robpduo@Robs-MacBook-Pro.local>
2022-07-10 20:22:11 -0400

    test getSchool method from engineer class

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,9 +1,15 @@
 const Engineer = require('../lib/Engineer');
 
 test('create engineer object', () => {
-    const engineer = new Engineer('Robert Engineer', 'robpduo@gmail.com');
+    const engineer = new Engineer('Robert Engineer', 'robpduo@gmail.com', 'University of Toronto');
 
     expect(engineer.name).toBe('Robert Engineer');
     expect(engineer.id).toBeLessThanOrEqual(1000);
     expect(engineer.id).toBeGreaterThanOrEqual(0);
+});
+
+test('retrieve engineers school name', () => {
+    const engineer = new Engineer('Robert Engineer', 'robpduo@gmail.com', 'University of Toronto');
+
+    expect(engineer.getSchool()).toBe('University of Toronto');
 });
