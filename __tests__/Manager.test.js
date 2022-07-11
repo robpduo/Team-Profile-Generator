c688717dd73106012e43438e6553f50723df6dd2
robpduo <robpduo@Robs-MacBook-Pro.local>
2022-07-10 20:08:10 -0400

    test constructor and getOffice methods from Manager class

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,1 +1,16 @@
 const Manager = require('../lib/Manager');
+
+test('create a manager object', () => {
+    const manager = new Manager('Robert Manager', 'robpduo@gmail.com');
+
+    expect(manager.name).toBe('Robert Manager');
+    expect(manager.email).toBe('robpduo@gmail.com');
+    expect(manager.id).toBeGreaterThan(0);
+});
+
+test('retrieve managers office number', () => {
+    const manager = new Manager('Robert Manager', 'robpduo@gmail.com');
+
+    expect(manager.getOffice()).toBeGreaterThanOrEqual(0);
+    expect(manager.getOffice()).toBeLessThanOrEqual(3);
+});
